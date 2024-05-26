717ed418baaae657432c9ae66a0a0eeacf96c225
hussain <hussainabdullatif@marun.edu.tr>
2024-05-26 21:47:07 +0300

    Implemented start new chat modal and search functionality
    - Update on Chat List Screen
    - Created a new custom component
    - Added modal for character selection

--- components/MyCharacterSelectionList.js
+++ components/MyCharacterSelectionList.js
@@ -1,23 +1,14 @@
 import React from "react";
 import { View, Text, Image, TouchableOpacity, FlatList } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
 import { getImage } from "../helpers";
 
-const MyVerticalCharactersList = ({ characters, favorites, onHeartPress }) => {
-    const navigation = useNavigation();
-
+const MyCharacterSelectionList = ({ characters, onCharacterSelect }) => {
     const renderCharacter = ({ item: character }) => {
-        const handleCharacterPress = () => {
-            // Navigate to ChatScreen when character is pressed
-            navigation.navigate("Chat", { character });
-        };
-
         return (
             <TouchableOpacity
                 className="bg-[#1F1F1F] mb-4 p-4 rounded-3xl"
                 activeOpacity={0.8}
-                onPress={handleCharacterPress}
+                onPress={() => onCharacterSelect(character)}
             >
                 {/* Image */}
                 <View className="flex-row">
@@ -31,25 +22,11 @@ const MyVerticalCharactersList = ({ characters, favorites, onHeartPress }) => {
                     <View className="ml-4 flex-1">
                         <Text className="text-white text-xl font-bold mb-1">{character.name}</Text>
                         <Text className="text-gray-400">
-                            {character.assistant_content.length > 80
-                                ? `${character.assistant_content.slice(0, 80)}...`
+                            {character.assistant_content.length > 60
+                                ? `${character.assistant_content.slice(0, 70)}...`
                                 : character.assistant_content}
                         </Text>
                     </View>
-
-                    {/* Heart icon */}
-                    <TouchableOpacity
-                        className="ml-4 justify-center"
-                        onPress={() => {
-                            onHeartPress(character?.id);
-                        }}
-                    >
-                        <MaterialCommunityIcons
-                            name={favorites.includes(character.id) ? "heart" : "heart-outline"}
-                            size={28}
-                            color="white"
-                        />
-                    </TouchableOpacity>
                 </View>
             </TouchableOpacity>
         );
@@ -60,9 +37,9 @@ const MyVerticalCharactersList = ({ characters, favorites, onHeartPress }) => {
             data={characters}
             renderItem={renderCharacter}
             keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={{ padding: 16 }}
+            showsVerticalScrollIndicator={false}
         />
     );
 };
 
-export default MyVerticalCharactersList;
+export default MyCharacterSelectionList;
